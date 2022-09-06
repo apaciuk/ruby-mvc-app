795721d103ec20b7fc065ea83925d456b13cd54b
Paul Anthony McGowan <info@xhostcom.com>
2022-09-06 20:44:49 +0100

    views and assets

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 class App 
     def call(env)
-        headers = { 'Content-Type' => 'text/json' }
+        headers = { 'Content-Type' => 'text/html' }
         return [200, headers, ['favicon']] if env['PATH_INFO'] == '/favicon.ico'
-        response = Rack::Response.new("Basic Ruby MVC App", 200, headers)
-        [response.status, response.headers, response.body]
+        response_html = File.read('views/index.html')
+        [200, headers, [response_html]]
     end
 end 
